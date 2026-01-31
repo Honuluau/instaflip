622366007233791d32e2f8b2d49c31cb68f36486
honuluau <honu.dylan@gmail.com>
2026-01-31 01:32:59 -0500

    Added Init and Close Func to back end

--- backend/database.go
+++ backend/database.go
@@ -1,9 +1,12 @@
 package backend
 
 import (
+	"database/sql"
 	"os"
 	"path/filepath"
 )
+
+var db *sql.DB
 
 func GetDBPath() string {
 	homeDir, err := os.UserHomeDir()
@@ -11,5 +14,44 @@ func GetDBPath() string {
 		homeDir = "."
 	}
 
-	return filepath.Join(homeDir, ".instaflip", "flips.db")
+	return filepath.Join(homeDir, ".instaflip", "instaflip.db")
+}
+
+func InitDB() error {
+	dbPath := GetDBPath()
+
+	// Create directory if it does not already exist.
+	dir := filepath.Dir(dbPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	var err error
+	db, err = sql.Open("sqlite3", dbPath)
+	if err != nil {
+		print(err.Error())
+		return err
+	}
+
+	// Create flips table.
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS flips {
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			eagle_id TEXT NOT NULL,
+			when DATETIME,
+		}
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Close DB Connection
+
+func CloseDB() {
+	if db != nil {
+		db.Close()
+	}
 }
