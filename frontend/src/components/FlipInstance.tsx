00a325b3866a9bc8395ed1ae01ae18ddeeaa43c8
honuluau <honu.dylan@gmail.com>
2026-02-11 08:38:14 -0500

    Added dropdown to flip instance.

--- frontend/src/components/FlipInstance.tsx
+++ frontend/src/components/FlipInstance.tsx
@@ -1,21 +1,38 @@
-import { EllipsisVertical } from "lucide-react";
+import { EllipsisVertical, Trash } from "lucide-react";
+import { useState } from "react";
 
 interface FlipInstanceProps {
     id: number;
     date: number;
 }
 
-export function FlipInstance({id, date}: FlipInstanceProps) {
+export function FlipInstance({ id, date }: FlipInstanceProps) {
+    const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
+    const handleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    }
+
     return (
         <div className='flip-instance'>
             <div className='selection'>
                 <span className='id'>{id}</span>
             </div>
             <div className='information'>
-                <span className='date'>{new Date(date).toLocaleString('en-us', {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'})}</span>
+                <span className='date'>{new Date(date).toLocaleString('en-us', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })}</span>
             </div>
             <div className='action'>
-                <button><EllipsisVertical size={16}/></button>
+                <button onClick={handleDropdown}><EllipsisVertical size={16} /></button>
+                {showDropdown && (
+                    <>
+                        <div className="dropdown">
+                            <button className="delete-btn">
+                                <Trash size={20}/>
+                                <span>Delete</span>
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     )
