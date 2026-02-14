1556711fe2e86fb59f5b303f73e8a1c36b60f530
honuluau <honu.dylan@gmail.com>
2026-02-14 14:22:21 -0500

    Title fixed to InstaFlip, capitalized F

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { version } from "../App";
 export function Header() {
     return (
         <div className="header">
-            <h1 className="m0">Instaflip</h1>
+            <h1 className="m0">InstaFlip</h1>
             <div className="badge"><p className="m0">{version}</p></div>
         </div>
     )
