34cb5b2ecd6d97a9e75b4da811a9b6bfab7cd555
honuluau <honu.dylan@gmail.com>
2026-02-14 13:40:20 -0500

    Added InfoPage.tsx because of Build Failure but removed all sensitive org information.

---
+++ frontend/src/pages/InfoPage.tsx
@@ -0,0 +1,17 @@
+export function InfoPage() {
+    return (
+        <div className="container no-top-margin flex-col justify-start">
+            <h1 className="page-header">Info</h1>
+            <div className="info-section">
+                <h2>Workflow</h2>
+                <div className="info-list">
+                    <p>1. Navigate to HOME page by clicking the house icon on the left.</p>
+                    <p>2. Click on the "Eagle Id" box in the "Flip Patron" section.</p>
+                    <p>3. Ask the patron to scan their eagle id.</p>
+                    <p>4. A banner will appear telling you if they are eligible to flip for the semester.</p>
+                    <p>5. If the patron is ineligible, the flip button will not appear. Cancel the flip and inform the patron that they have reached the maximum.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
