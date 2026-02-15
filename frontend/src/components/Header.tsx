45c65944eac6586a9b6cdd7f00e34c1afea8c876
honuluau <honu.dylan@gmail.com>
2026-02-15 16:35:56 -0500

    Uploaded custom logo.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { version } from "../App";
+import logo from "../assets/images/InstaFlip-Logo.png";
 
 export function Header() {
     return (
         <div className="header">
+            <img src={logo} width={40}/>
             <h1 className="m0">InstaFlip</h1>
             <div className="badge"><p className="m0">{version}</p></div>
         </div>
