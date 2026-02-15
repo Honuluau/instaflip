c701b330a3b6b6e40d09f57f4e3602df1a2bcf99
honuluau <honu.dylan@gmail.com>
2026-02-15 16:36:26 -0500

    dev_0.8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ import { CanFlipBanner, MaximumFlipsReachedBanner } from './components/Banners';
 import { HomePage } from './pages/HomePage';
 import { ToastContainer } from './components/ToastContainer';
 
-export const version = "dev_0.7"
+export const version = "dev_0.8"
 
 function App() {
     const [currentPage, setCurrentPage] = useState<PageType>("main");
