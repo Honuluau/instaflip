9a5e7d09717b731b7ee394ffdd7e941b296bde8a
honuluau <honu.dylan@gmail.com>
2026-02-15 17:07:54 -0500

    Changed instaflip to InstaFlip

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:     "instaflip",
+		Title:     "InstaFlip",
 		Width:     1024,
 		Height:    768,
 		Frameless: true,
