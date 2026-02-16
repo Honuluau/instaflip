3666507c02df5daf7288648068f9740b73b64ee1
honuluau <honu.dylan@gmail.com>
2026-02-16 07:58:59 -0500

    v1.0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ import { CanFlipBanner, MaximumFlipsReachedBanner } from './components/Banners';
 import { HomePage } from './pages/HomePage';
 import { ToastContainer } from './components/ToastContainer';
 
-export const version = "dev_0.8"
+export const version = "v1.0"
 
 function App() {
     const [currentPage, setCurrentPage] = useState<PageType>("main");
--- frontend/src/pages/InfoPage.tsx
+++ frontend/src/pages/InfoPage.tsx
@@ -4,11 +4,12 @@ export function InfoPage() {
             <h1 className="page-header">Info</h1>
             <div className="info-section">
                 <h2>About</h2>
-                <p>InstaFlip is a policy at Zach Henderson Library to speed up the process of renewing a laptop. Typically, a patron who wants to return and checkout another laptop will recieve a seperate laptop. If there are no laptops available, the patron would have to wait for a staff member to process the laptop.</p>
+                <p>InstaFlip is a policy at Zach Henderson Library to speed up the process of renewing a laptop. Typically, a patron who wants to return and checkout another laptop will recieve a seperate laptop. If there are no laptops available, the patron would have to wait for a staff member to process the laptop. InstaFlip allows patrons to return and check out the same laptop with no questions asked.</p>
             </div>
             <div className="info-section">
                 <h2>Workflow</h2>
                 <div className="info-list">
+                    <p>When a patron is returning a laptop and they say that they want to check it back out, inform them of InstaFlip.</p>
                     <p>1. Navigate to HOME page by clicking the house icon on the left.</p>
                     <p>2. Click on the "Eagle Id" box in the "Flip Patron" section.</p>
                     <p>3. Ask the patron to scan their eagle id.</p>
