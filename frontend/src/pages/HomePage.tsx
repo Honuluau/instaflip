42b27ac3df30fdefa3b30edfed432201fa41e0d2
honuluau <honu.dylan@gmail.com>
2026-02-17 08:53:31 -0500

    Disabled takes priority over Semester out of date range

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -121,7 +121,7 @@ export function HomePage() {
 
     return (
         <>
-            {outOfDate ? (
+            {(outOfDate && !settings.disabled) ? (
                 <>
                     <Overlay icon={<Calendar />} label="Semester Range is out of date." text="Please change the semester settings by clicking the calendar button on the left-side of this window. The semester range is derived from Academic Calendars. Academic Calendars are available through the Office of the Registrar on Georgia Southern's website." />
                 </>
