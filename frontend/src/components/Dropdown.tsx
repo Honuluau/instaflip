520ff4ccd89272151c860260a0db96d19c0b23f8
honuluau <honu.dylan@gmail.com>
2026-02-18 09:47:16 -0500

    Added dropdown component and implemented it with Statistics

---
+++ frontend/src/components/Dropdown.tsx
@@ -0,0 +1,45 @@
+import { Check, ChevronDown, Download } from "lucide-react";
+import { useState } from "react"
+
+interface DropdownProps {
+    options: string[]
+}
+
+export function Dropdown({ options }: DropdownProps) {
+    const [showDropdown, setShowDropdown] = useState<Boolean>(false);
+    const [selectedOption, setSelectedOption] = useState<string>(options[0]);
+
+    const handleSelect = (newOption: string) => {
+        setSelectedOption(newOption);
+        console.log(newOption);
+    }
+
+    const handleShow = () => {
+        setShowDropdown(!showDropdown);
+    }
+
+    return (
+        <>
+            <button className="dropdown-button" onClick={handleShow}>
+                <Download size={20} />
+                <span>{selectedOption}</span>
+                <ChevronDown size={20} />
+            </button>
+            {(showDropdown) ? (<></>) : (
+                <>
+                    <div className="dropdown-list">
+                        {options.map((option, index) => (
+                            <button key={index} onClick={() => handleSelect(option)}>
+                                <span>{option}</span>
+                                {(option == selectedOption) ? (
+                                    <>
+                                        <Check size={16} />
+                                    </>) : (<></>)}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            )}
+        </>
+    )
+}
--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Datebox } from "../components/Datebox";
 import { getSettings, Settings as SettingsType } from "../lib/settings";
-import { Bell, Download } from "lucide-react";
+import { Ban, Bell, Download, Repeat } from "lucide-react";
 import { ExportStatistics } from "../../wailsjs/go/main/App";
 import { logger } from "../lib/logger";
 import { toaster } from "../lib/toaster";
+import { Switch } from "../components/Switch";
+import { Dropdown } from "../components/Dropdown";
 
 export function StatisticsPage() {
     const [savedSettings, setSavedSettings] = useState<SettingsType>(getSettings());
@@ -32,22 +34,47 @@ export function StatisticsPage() {
     return (
         <div className="container no-top-margin flex-col justify-start">
             <h1 className="page-header">Statistics</h1>
-            <h2>Data Range</h2>
-            <div className="semester-date-settings">
-                <div>
-                    <h4>Export Start</h4>
-                    <Datebox date={new Date(savedSettings.semesterStart)} update={handleExportStart} />
-                </div>
-                <div>
-                    <h4>Export End</h4>
-                    <Datebox date={new Date(savedSettings.semesterEnd)} update={handleExportEnd}/>
+            <div className="flex-col">
+                <h2>Tables</h2>
+                <div className="flex-col">
+                    <div className="flex-row statistic-switch">
+                        <Repeat size={20} />
+                        <h4>Flips</h4>
+                        <Switch />
+                    </div>
+                    <div className="flex-row statistic-switch">
+                        <Ban />
+                        <h4>Declines</h4>
+                        <Switch />
+                    </div>
                 </div>
             </div>
-            <div>
-                <button className="accent check-btn no-margin" onClick={onExport}>
-                    <Download size={20}/>
-                    <span>Export .csv</span>
-                </button>
+
+            <div className="flex-row">
+                <div className="flex-col">
+                    <h2>Export Style</h2>
+                    <Dropdown options={["Unix Milliseconds", "Datetime", "Year, Month, & Timestamp"]} />
+                </div>
+            </div>
+
+            <div className="flex-col">
+                <h2>Data Range</h2>
+                <div className="semester-date-settings">
+                    <div>
+                        <h4>Export Start</h4>
+                        <Datebox date={new Date(savedSettings.semesterStart)} update={handleExportStart} />
+                    </div>
+                    <div>
+                        <h4>Export End</h4>
+                        <Datebox date={new Date(savedSettings.semesterEnd)} update={handleExportEnd} />
+                    </div>
+                </div>
+                <div>
+                    <button className="accent check-btn no-margin" onClick={onExport}>
+                        <Download size={20} />
+                        <span>Export .csv</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
