276b3868d3389ffe4eea9ea57067b426531d9ef3
honuluau <honu.dylan@gmail.com>
2026-02-23 09:48:29 -0500

    Added CSV Datetime Style

--- backend/exports.go
+++ backend/exports.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -48,9 +49,13 @@ func SelectFolderDialog(ctx context.Context, defaultPath string) (string, error)
 	return selectedPath, nil
 }
 
-func ExportCSV(startEpoch, endEpoch int64, path, table string) (bool, error) {
-	// Get rows, write to csv.
-	return false, nil
+func GenerateFileName() string {
+	now := time.Now().Format("2006-01-02_15-04-05")
+	return "InstaFlip Export (" + now + ")"
+}
+
+func GetExportPath(path, fileExtension string) string {
+	return filepath.Join(path, GenerateFileName()+"."+fileExtension)
 }
 
 func ConvertUnixMillisecondsToDateString(unixMilliseconds int64) string {
@@ -58,10 +63,16 @@ func ConvertUnixMillisecondsToDateString(unixMilliseconds int64) string {
 	return t.Format("2006-01-02")
 }
 
-func WriteUnixMillisecondsXLSX(table string, rows []FlipRowItem, f *excelize.File) bool {
+func ConvertUnixMillisecondsToTimestamp(unixMilliseconds int64) string {
+	t := time.UnixMilli(unixMilliseconds)
+	return t.Format("2006-01-02 15:04:05 MST")
+}
+
+func CreateNewSheetOrOverwriteExisting(table string, f *excelize.File) error {
 	index, err := f.GetSheetIndex(table)
 	if err != nil {
 		fmt.Println(err)
+		return err
 	}
 
 	// Create new or overwrite existing sheet and set to active.
@@ -71,14 +82,25 @@ func WriteUnixMillisecondsXLSX(table string, rows []FlipRowItem, f *excelize.Fil
 		index, err := f.NewSheet(table)
 		if err != nil {
 			fmt.Println(err)
+			return err
 		}
 		f.SetActiveSheet(index)
+	}
+
+	return nil
+}
+
+func WriteUnixMillisecondsXLSX(table string, rows []FlipRowItem, f *excelize.File) bool {
+	err := CreateNewSheetOrOverwriteExisting(table, f)
+	if err != nil {
+		fmt.Println(err)
+		return false
 	}
 
 	// Set headers
 	f.SetCellValue(table, "A1", "InstaFlip Id")
 	f.SetCellValue(table, "B1", "Eagle Id")
-	f.SetCellValue(table, "C1", "Unix Milliseconds")
+	f.SetCellValue(table, "C1", "Unix Millisecond")
 
 	// Paste Data
 	for index, row := range rows {
@@ -102,12 +124,67 @@ func WriteUnixMillisecondsXLSX(table string, rows []FlipRowItem, f *excelize.Fil
 	return true
 }
 
+func WriteDatetimeXLSX(table string, rows []FlipRowItem, f *excelize.File) error {
+	err := CreateNewSheetOrOverwriteExisting(table, f)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// Set Headers
+	f.SetCellValue(table, "A1", "InstaFlip Id")
+	f.SetCellValue(table, "B1", "Eagle Id")
+	f.SetCellValue(table, "C1", "Datetime")
+
+	// Paste Data
+	for index, row := range rows {
+		f.SetCellValue(table, "A"+strconv.Itoa(index+2), row.ID)
+		f.SetCellValue(table, "B"+strconv.Itoa(index+2), row.EagleID)
+		f.SetCellValue(table, "C"+strconv.Itoa(index+2), ConvertUnixMillisecondsToTimestamp(row.FlipTime))
+	}
+
+	f.SetColWidth(table, "B", "B", 16)
+	f.SetColWidth(table, "C", "C", 30)
+
+	return nil
+}
+
+func WriteYearMonthTimestamp(table string, rows []FlipRowItem, f *excelize.File) error {
+	err := CreateNewSheetOrOverwriteExisting(table, f)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// Set Headers
+	f.SetCellValue(table, "A1", "InstaFlip Id")
+	f.SetCellValue(table, "B1", "Eagle Id")
+	f.SetCellValue(table, "C1", "Year")
+	f.SetCellValue(table, "D1", "Month")
+	f.SetCellValue(table, "E1", "Timestamp")
+
+	// Paste Data
+	for index, row := range rows {
+		f.SetCellValue(table, "A"+strconv.Itoa(index+2), row.ID)
+		f.SetCellValue(table, "B"+strconv.Itoa(index+2), row.EagleID)
+
+		t := time.UnixMilli(row.FlipTime)
+		f.SetCellValue(table, "C"+strconv.Itoa(index+2), t.Year())
+		f.SetCellValue(table, "D"+strconv.Itoa(index+2), t.Month())
+
+		f.SetCellValue(table, "E"+strconv.Itoa(index+2), ConvertUnixMillisecondsToTimestamp(row.FlipTime))
+	}
+
+	f.SetColWidth(table, "B", "B", 16)
+	f.SetColWidth(table, "C", "C", 6)
+	f.SetColWidth(table, "D", "D", 12)
+	f.SetColWidth(table, "E", "E", 30)
+
+	return nil
+}
+
 func ExportXLSX(startEpoch, endEpoch int64, path, style string, tables []string) (bool, error) {
 	rows, _ := GetRowsOfTable(startEpoch, endEpoch, tables)
-	fmt.Println(rows)
-	fmt.Println(rows["declines"][0])
-	// Returns dictionary of tables with rows of FlipRowItems
-	// Just find a way to export the data now.
 
 	// Create a new Excel File
 	f := excelize.NewFile()
@@ -120,9 +197,18 @@ func ExportXLSX(startEpoch, endEpoch int64, path, style string, tables []string)
 	// Initialize the first Sheet with the first index of tables. The write functions should overwrite data here.
 	f.SetSheetName("Sheet1", tables[0])
 
-	if style == "Unix Milliseconds" {
+	switch style {
+	case "Unix Milliseconds":
 		for _, table := range tables {
 			WriteUnixMillisecondsXLSX(table, rows[table], f)
+		}
+	case "Datetime":
+		for _, table := range tables {
+			WriteDatetimeXLSX(table, rows[table], f)
+		}
+	case "Year, Month, & Timestamp":
+		for _, table := range tables {
+			WriteYearMonthTimestamp(table, rows[table], f)
 		}
 	}
 
@@ -142,7 +228,7 @@ func ExportXLSX(startEpoch, endEpoch int64, path, style string, tables []string)
 	f.SetColWidth("info", "A", "B", 16)
 
 	f.SetActiveSheet(0)
-	if err := f.SaveAs(filepath.Join(path, "InstaFlip.xlsx")); err != nil {
+	if err := f.SaveAs(GetExportPath(path, "xlsx")); err != nil {
 		fmt.Println(err)
 		return false, nil
 	}
@@ -150,15 +236,66 @@ func ExportXLSX(startEpoch, endEpoch int64, path, style string, tables []string)
 	return false, nil
 }
 
-func Export(startEpoch, endEpoch int64, path, style, fileType string, tables []string) (bool, error) {
-	if len(tables) == 1 {
-		// Export 1 table as either a .csv or an .xlsx
-		ExportCSV(startEpoch, endEpoch, path, tables[0])
-	} else {
-		// It is impossible to have 0 tables, therefore everything exported here will be separate .xlsx sheets.
-		ExportXLSX(startEpoch, endEpoch, path, style, tables)
+func WriteUnixMillisecondsCSV(file *os.File, rows []FlipRowItem, table string) {
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"InstaFlip Id", "Eagle Id", "Unix Millisecond (" + table + ")"})
+
+	for _, row := range rows {
+		writer.Write([]string{
+			strconv.FormatInt(row.ID, 10),
+			row.EagleID,
+			strconv.FormatInt(row.FlipTime, 10),
+		})
 	}
-	fmt.Println("Recieved:", startEpoch, endEpoch, path)
-	success, err := ExportRange(startEpoch, endEpoch, path)
-	return success, err
+}
+
+func WriteDatetimeCSV(file *os.File, rows []FlipRowItem, table string) {
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"InstaFlip Id", "Eagle Id", "Datetime (" + table + ")"})
+
+	for _, row := range rows {
+		writer.Write([]string{
+			strconv.FormatInt(row.ID, 10),
+			row.EagleID,
+			ConvertUnixMillisecondsToDateString(row.FlipTime),
+		})
+	}
+}
+
+func ExportCSV(startEpoch, endEpoch int64, path, style, table string) {
+	tables := []string{table}
+	rows, err := GetRowsOfTable(startEpoch, endEpoch, tables)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fullPath := GetExportPath(path, "csv")
+	file, err := os.Create(fullPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	switch style {
+	case "Unix Milliseconds":
+		WriteUnixMillisecondsCSV(file, rows[table], table)
+	case "Datetime":
+		WriteDatetimeCSV(file, rows[table], table)
+	}
+}
+
+func Export(startEpoch, endEpoch int64, path, style, fileType string, tables []string) (bool, error) {
+	switch fileType {
+	case "xlsx":
+		ExportXLSX(startEpoch, endEpoch, path, style, tables)
+	case "csv":
+		ExportCSV(startEpoch, endEpoch, path, style, tables[0])
+	}
+
+	return true, nil
 }
